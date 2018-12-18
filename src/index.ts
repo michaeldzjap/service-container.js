2c9c517dac1d31795abddd8053b081c2db0ca79f
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2018-12-18 20:39:43 +0100

    Expose additional classes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 
 import Arr from './Support/Arr';
+import Callable from './Container/Callable';
 import Container from './Container/Container';
 import InterfaceFactory from './Support/InterfaceFactory';
 import injectable from './Support/injectable';
 
 export {
     Arr,
+    Callable,
     Container,
     InterfaceFactory,
     injectable
