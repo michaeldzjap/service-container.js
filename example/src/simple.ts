80d19b62bf59cb93356638c42d2d0034681dc1c4
Michael Dzjaparidze <michael.dzjaparidze@gmail.com>
2018-12-26 18:06:40 +0100

    Fix comment

--- example/src/simple.ts
+++ example/src/simple.ts
@@ -3,4 +3,4 @@ import {Container} from '../../dist/container';
 const container = new Container;
 
 container.bind('foo', (): string => 'bar');
-const result = container.make('foo'); // --> 'foo'
+const result = container.make('foo'); // --> 'bar'
